73c37893446480a9d90d8008ac260bfeb8350169
Adrian <Adrian@10.0.2.15>
2017-02-10 14:47:00 -0600

    hasta el 5/2

---
+++ Tarea05/src/ejerciciosTarea05/MaquinaAgricultura.java
@@ -0,0 +1,27 @@
+/**
+ * Maquina Agricultura
+ * Descripción: Define si encender o no una maquina rociadora
+ * Fecha:10 de febrero del 2017
+ * Autor:Adrián Obando Leitón
+ * Fecha de modificación: 10-02-17
+ * Modificado por: Adrián Obando Leitón
+ */
+package ejerciciosTarea05;
+
+//Librerias
+
+import java.io.*;
+
+public class MaquinaAgricultura {
+
+    public static void main(String[] args) throws IOException {//Inicia el programa
+        //Variables
+        String clima;
+        int cultivo,tiemSie,temp,tiempCos;
+
+        //variables de entrada y salida
+        BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
+        PrintStream imprimir = System.out;
+
+    }//Termina el programa
+}
