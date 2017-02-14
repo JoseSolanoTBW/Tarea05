9b0d4e53a8ae5f94e1ee6f02b8281d4335336359
jtest <jtest@JoseTest.WSI.com>
2017-02-14 14:56:53 -0800

    Num Inverso

--- Tarea05/src/ejerciciosTarea05/NumeroInverso.java
+++ Tarea05/src/ejerciciosTarea05/NumeroInverso.java
@@ -1,6 +1,6 @@
 /**
- * Determinar la distancia segun dos puntos
- * Descripción: Dados 2 puntos en el plano cartesiano 
+ * Determinar el inverso de un numero
+ * Descripción: Dado 1 numero poner dicho numero alrevez
  * Fecha: 13-2-2017
  * Autor: Jose Solano Montoya
  * Fecha de modificación: 13-02-17
@@ -15,12 +15,32 @@ public class NumeroInverso {
     public static void main (String[] args) throws IOException
     {
         //Variables
-        int i, num1, numInver;
+        int i, num1,num2, numInver, cont=0, numD=10, suma=0,expon, multi=1;
         
         BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
         PrintStream imprimir = System.out;
         
-        
+        imprimir.println("Digite un numero");
+        num1= Integer.parseInt(leer.readLine());
+        imprimir.println("Su inverso es: ");
+        num2=num1;
+        while(num2!=0)
+        {
+            num2=num2/10;
+            cont=cont+1;
+        }
+      
+        while (cont!=0)
+        {
+            expon= cont;
+            numInver=num1 %10;
+            while(expon!=0)
+            {
+             multi= multi*10;    
+            }
+            suma=suma+(multi*numInver);
+        }
+        imprimir.println(suma);
         
     }
 }
